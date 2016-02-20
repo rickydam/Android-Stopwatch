8928f4bf00d20d9ce3553a688ff7086eb414a1c0
Ricky Dam <Ricky Dam>
2016-02-20 12:30:19 -0500

    Implemented the start and pause buttons

--- app/src/main/java/ricky/timer/MainActivity.java
+++ app/src/main/java/ricky/timer/MainActivity.java
@@ -1,13 +1,68 @@
 package ricky.timer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;      // Updates the value of the timer
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private TextView timerValue;
+    private long startTime = 0L;
+    private Handler customHandler = new Handler();
+
+    long timeInMilliseconds = 0L;
+    long timeSwapBuff = 0L;
+    long updatedTime = 0L;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
+
+        Button startButton;
+        Button pauseButton;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timerValue = (TextView) findViewById (R.id.timerValue);
+
+        startButton = (Button)  findViewById (R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+           public void onClick(View view) {
+               startTime = SystemClock.uptimeMillis();
+               customHandler.postDelayed(updateTimerThread, 0);
+           }
+        });
+
+        pauseButton = (Button) findViewById (R.id.pauseButton);
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                timeSwapBuff += timeInMilliseconds;
+                customHandler.removeCallbacks(updateTimerThread);
+            }
+        });
     }
+
+    private Runnable updateTimerThread = new Runnable() {
+      public void run() {
+          timeInMilliseconds = SystemClock.uptimeMillis() - startTime;
+          updatedTime = timeSwapBuff + timeInMilliseconds;
+
+          int seconds = (int) (updatedTime / 1000);
+          int minutes = seconds / 60;
+          seconds = seconds % 60;
+          int milliseconds = (int) (updatedTime % 1000);
+
+          String string = "";
+          string += "" + minutes;
+          string += ":" + String.format("%02d", seconds);
+          string += ":" + String.format("%03d", milliseconds);
+
+          timerValue.setText(string);
+          customHandler.postDelayed(this, 0);
+      }
+    };
 }
